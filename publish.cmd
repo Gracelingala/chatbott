92e7196bde93467c03b0561d309bfcaa6bce441f
Gracelingala <111064427+Gracelingala@users.noreply.github.com>
2022-11-05 18:55:07 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\gracegg123-bot-8b69-Web-Deploy.pubxml -p:Password=dGGDnEpiy5RqG5eMLlm6sWzKfmpf99Tjb9KoZSb3trj8wyjtP7QDahAmtoCg
+
